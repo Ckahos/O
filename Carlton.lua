8c3442d746ef6f48b18024957ca076f85e2ccf7b
Ckahos <thialisson.caaci@gmail.com>
2017-05-16 14:37:19 -0300

    Add files via upload

---
+++ Carlton.lua
@@ -0,0 +1,17 @@
+function lol(a,b,c,d,e)
+return {
+   (a*b*(c^(d*e)))/(a+(b*(c^(d*e)-1)))
+}
+end
+function ll()
+local pui = {}
+print("Insert the initial condition!")
+table.insert(pui,1,io.read())
+print("Insert the carrying capacity!")
+table.insert(pui,2,io.read())
+print("Insert the growth rate!")
+table.insert(pui,3,io.read())
+print("Insert the time of measure!")
+table.insert(pui,4,io.read())
+print("The final population size is: "..lol(pui[2],pui[1],pui,pui[3],pui[4]))
+end
