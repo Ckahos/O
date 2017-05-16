683e4191531c6cdec70176e47d2e8818f6b486f6
Ckahos <thialisson.caaci@gmail.com>
2017-05-16 14:47:16 -0300

    Update Carlton.lua

--- Carlton.lua
+++ Carlton.lua
@@ -1,10 +1,8 @@
 function lol(a,b,c,d,e)
-return {
-   (a*b*(c^(d*e)))/(a+(b*(c^(d*e)-1)))
-}
+return (a*b*(c^(d*e)))/(a+(b*(c^(d*e)-1)))
 end
 function ll()
-local e = 2,7182818284590452353602874713527
+pui["e"] = 2,7182818284590452353602874713527
 local pui = {}
 print("Insert the initial condition!")
 table.insert(pui,1,io.read())
@@ -14,5 +12,5 @@ print("Insert the growth rate!")
 table.insert(pui,3,io.read())
 print("Insert the time of measure!")
 table.insert(pui,4,io.read())
-print("The final population size is: "..lol(pui[2],pui[1],e,pui[3],pui[4]))
+print("The final population size is: "..lol(pui[2],pui[1],pui["e"],pui[3],pui[4]))
 end
