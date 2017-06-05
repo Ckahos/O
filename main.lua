44a3fd940c1e80fb7c27305aaee6c311fd988fab
Ckahos <thialisson.caaci@gmail.com>
2017-06-05 15:17:51 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,27 +5,28 @@ love.physics.setMeter(20)
 langdon = love.physics.newWorld(0,0, true)
 xw,yw = love.graphics.getDimensions()
 langdon:setCallbacks(beginContact,endContact,preSolve,postSolve)
-a = {}
-a.body = love.physics.newBody(langdon,xw/8,yw/2, "dynamic")
-a.shape = love.physics.newRectangleShape(200, 40)
-a.f = love.physics.newFixture(a.body,a.shape)
-a.f:setUserData("Ship")
-b = {}
-b.body = love.physics.newBody(langdon,xw/2,yw/2, "dynamic")
-b.shape = love.physics.newRectangleShape(20,20)
-b.f = love.physics.newFixture(b.body,b.shape)
-b.f:setUserData("Meteor") 
+ship = {}
+ship.body = love.physics.newBody(langdon,xw/8,yw/2, "dynamic")
+ship.body:setMass(10)
+ship.shape = love.physics.newRectangleShape(200, 40)
+ship.f = love.physics.newFixture(a.body,a.shape)
+ship.f:setUserData("Ship")
+meteor = {}
+meteor.body = love.physics.newBody(langdon,xw/2,yw/2, "dynamic")
+meteor.shape = love.physics.newRectangleShape(20,20)
+meteor.f = love.physics.newFixture(b.body,b.shape)
+meteor.f:setUserData("Meteor") 
 text = ""
 persisting = 0
 end 
 function love.draw()
 love.graphics.setBackgroundColor(0,0,0)
 love.graphics.setColor(255,255,255,255)
-love.graphics.print(a.body:getX()..'||'.. a.body:getY())
+love.graphics.print(ship.body:getX()..'||'.. ship.body:getY())
 love.graphics.setColor(100,0,255)
-love.graphics.polygon("fill",a.body:getWorldPoints(a.shape:getPoints()))
+love.graphics.polygon("fill",ship.body:getWorldPoints(ship.shape:getPoints()))
 love.graphics.setColor(255,255,255)
-love.graphics.polygon( "fill",b.body:getWorldPoints(b.shape:getPoints()))
+love.graphics.polygon( "fill",meteor.body:getWorldPoints(meteor.shape:getPoints()))
 love.graphics.setColor(255,255,255)
 love.graphics.print(text,10,10) 
 end
@@ -35,9 +36,9 @@ langdon:update(dt)
      text = ""
    end
   if love.keyboard.isDown('up') then
-      a.body:setPosition(a.body:getX(), a.body:getY() - 7)
+      a.body:setPosition(ship.body:getX(), ship.body:getY() - 7)
     elseif love.keyboard.isDown('down') then
-      a.body:setPosition(a.body:getX(),a.body:getY() + 7)
+      a.body:setPosition(ship.body:getX(),ship.body:getY() + 7)
   end
    b.body:setPosition(b.body:getX() - 4,b.body:getY())
 end
