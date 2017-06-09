66b979c8b45748507b58a08ddd85e2ff308c8512
Ckahos <thialisson.caaci@gmail.com>
2017-06-09 16:43:41 -0300

    (incomplete)

---
+++ HTHT.java
@@ -0,0 +1,19 @@
+class medium {
+     String m_types[] = {"Chaotic","Quiescent","Krommmic"}
+     
+}
+class cell extends medium {
+    double speed, imc, radius,
+}
+public class HTHT extends cell, {
+    private static void dd(String a) {
+         System.out.println(a);
+     }
+      private static void ff(double a) {
+            System.out.println(a)
+      }
+  
+  public static void main(String[] args){
+      
+  }
+  }
