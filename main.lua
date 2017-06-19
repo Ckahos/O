870ab75770a03228804e399a6972fb617e36cf23
Ckahos <thialisson.caaci@gmail.com>
2017-06-19 18:33:08 -0300

    Sample of the third game(incomplete)

--- main.lua
+++ main.lua
@@ -1,63 +1,41 @@
 function love.load()
 love.window.setFullscreen(true,"desktop")
-love.window.setTitle("Meteor jam!")
 love.physics.setMeter(20)
-langdon = love.physics.newWorld(0,0, true)
+langdon = love.physics.newWorld(0,15.089*20, true)
 xw,yw = love.graphics.getDimensions()
-langdon:setCallbacks(beginContact,endContact,preSolve,postSolve)
-ship = {}
-ship.body = love.physics.newBody(langdon,xw/8,yw/2, "dynamic")
-ship.body:setMass(10)
-ship.shape = love.physics.newRectangleShape(200, 40)
-ship.f = love.physics.newFixture(a.body,a.shape)
-ship.f:setUserData("Ship")
-meteor = {}
-meteor.body = love.physics.newBody(langdon,xw/2,yw/2, "dynamic")
-meteor.shape = love.physics.newRectangleShape(20,20)
-meteor.f = love.physics.newFixture(b.body,b.shape)
-meteor.f:setUserData("Meteor") 
-text = ""
-persisting = 0
-end 
+hun = {}
+hun.body = love.physics.newBody(langdon,xw/2,yw/2, "dynamic")
+hun.shape = love.physics.newCircleShape(20)
+hun.f = love.physics.newFixture(hun.body,hun.shape)
+hun.f:getRestitution(0.89976)
+ujj = {}
+ujj.body = love.physics.newBody(langdon, xw/2, yw * 2)
+ujj.shape = love.physics.newRectangleShape(200,400)
+ujj.f = love.physics.newFixture(ujj.body,ujj.shape)
+ujj.f:setRestitution(0.89098)
+r_1 = {}
+r_1.body = love.physics.newBody(langdon,xw/4, yw/3)
+r_1.shape = love.physics.newRectangleShape(200,200)
+r_1.f = love.physics.newFixture(r_1.body,r_1.shape)
+r_1.f:setRestitution(0.987678)
+end
 function love.draw()
-love.graphics.setBackgroundColor(0,0,0)
-love.graphics.setColor(255,255,255,255)
-love.graphics.print(ship.body:getX()..'||'.. ship.body:getY())
-love.graphics.setColor(100,0,255)
-love.graphics.polygon("fill",ship.body:getWorldPoints(ship.shape:getPoints()))
-love.graphics.setColor(255,255,255)
-love.graphics.polygon( "fill",meteor.body:getWorldPoints(meteor.shape:getPoints()))
-love.graphics.setColor(255,255,255)
-love.graphics.print(text,10,10) 
+love.graphics.setBackgroundColor(255,255,255,255)
+love.graphics.setColor(0,0,0)
+love.graphics.circle("line",hun.body:getX(),hun.body:getY(),hun.shape:getRadius())
+love.graphics.setColor(0,0,0)
+love.graphics.polygon("line",ujj.body:getWorldPoints(ujj.shape:getPoints()))
+love.graphics.setColor(255,0,121)
+love.graphics.print(hun.body:getX().. "||".. hun.body:getY())
+love.graphics.setColor(0,0,0)
+love.graphics.polygon("line",r_1.body:getWorldPoints(r_1.shape:getPoints()))
+love.graphics.print(xw .. "||".. yw,0,20)
 end
 function love.update(dt)
 langdon:update(dt)
-  if string.len(text) > 768 then 
-     text = ""
-   end
-  if love.keyboard.isDown('up') then
-      a.body:setPosition(ship.body:getX(), ship.body:getY() - 7)
-    elseif love.keyboard.isDown('down') then
-      a.body:setPosition(ship.body:getX(),ship.body:getY() + 7)
-  end
-   b.body:setPosition(b.body:getX() - 4,b.body:getY())
-end
-function beginContact(a,b,coll)
-x,y = coll:getNormal()
-text = text .. "\n" .. a:getUserData() .. " colliding with " .. b:getUserData() .. " with a vector normal of: " .. x .. ", " ..y
-end
-function endContact(a,b,coll)
-persisting = 0
-text = text .. "\n" ..a:getUserData() .. " uncolliding with " .. b:getUserData() 
-end
-function preSolve(a,b,coll)
- if persisting ==0 then 
-     text = text .."\n"..a:getUserData() .. " touching " .. b:getUserData()
-     elseif persisting < 20 then
-         text = text .. " ".. persisting 
-end
-persisting = persisting + 1 
-end
-function postSolve(a,b,coll,normalimpulse,tangentimpulse)
-end
--- There's still the need to use the callbacks correctly....
+if love.keyboard.isDown('right') then
+    hun.body:applyForce(1000,0) 
+elseif love.keyboard.isDown('left') then
+     hun.body:applyForce(- 1000,0)
+     end
+     end
