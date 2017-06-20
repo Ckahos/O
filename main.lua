38cbe6ead697d7d83bb746ca0fc295b0200270e0
Ckahos <thialisson.caaci@gmail.com>
2017-06-20 16:05:06 -0300

    Ball game with callbacks(incomeplete )

--- main.lua
+++ main.lua
@@ -1,41 +1,58 @@
 function love.load()
 love.window.setFullscreen(true,"desktop")
-love.physics.setMeter(20)
-langdon = love.physics.newWorld(0,15.089*20, true)
 xw,yw = love.graphics.getDimensions()
-hun = {}
-hun.body = love.physics.newBody(langdon,xw/2,yw/2, "dynamic")
-hun.shape = love.physics.newCircleShape(20)
-hun.f = love.physics.newFixture(hun.body,hun.shape)
-hun.f:getRestitution(0.89976)
-ujj = {}
-ujj.body = love.physics.newBody(langdon, xw/2, yw * 2)
-ujj.shape = love.physics.newRectangleShape(200,400)
-ujj.f = love.physics.newFixture(ujj.body,ujj.shape)
-ujj.f:setRestitution(0.89098)
-r_1 = {}
-r_1.body = love.physics.newBody(langdon,xw/4, yw/3)
-r_1.shape = love.physics.newRectangleShape(200,200)
-r_1.f = love.physics.newFixture(r_1.body,r_1.shape)
-r_1.f:setRestitution(0.987678)
+love.physics.setMeter(20)
+langdon = love.physics.newWorld(0,25*20, true)
+langdon:setCallbacks(beginContact,endContact,preSolve,postSolve)
+    ball = {}
+    ball.body = love.physics.newBody(langdon,xw/2,yw/2, "dynamic")
+    ball.shape = love.physics.newCircleShape(20)
+    ball.f = love.physics.newFixture(ball.body, ball.shape)
+    ball.f:setRestitution(0.7)
+    ball.f:setUserData("Ball")
+    block = {}
+    block.body = love.physics.newBody(langdon,xw/2,yw+100)
+    block.shape = love.physics.newRectangleShape(400,500)
+    block.f = love.physics.newFixture(block.body,block.shape)
+    block.f:setRestitution(0.7)
+    block.f:setUserData("Block")
+     text = ""
+     persisting = 0
 end
 function love.draw()
-love.graphics.setBackgroundColor(255,255,255,255)
-love.graphics.setColor(0,0,0)
-love.graphics.circle("line",hun.body:getX(),hun.body:getY(),hun.shape:getRadius())
-love.graphics.setColor(0,0,0)
-love.graphics.polygon("line",ujj.body:getWorldPoints(ujj.shape:getPoints()))
-love.graphics.setColor(255,0,121)
-love.graphics.print(hun.body:getX().. "||".. hun.body:getY())
-love.graphics.setColor(0,0,0)
-love.graphics.polygon("line",r_1.body:getWorldPoints(r_1.shape:getPoints()))
-love.graphics.print(xw .. "||".. yw,0,20)
+love.graphics.setColor(255,255,255)
+love.graphics.circle("line", ball.body:getX(),ball.body:getY(),ball.shape:getRadius())
+love.graphics.setColor(255,255,255)
+love.graphics.polygon("line",block.body:getWorldPoints(block.shape:getPoints()))
+--love.graphics.print(xw .. "||".. yw)
+--love.graphics.print(love.system.getOS(),0,23)
+love.graphics.print(text,xw,yw)
 end
 function love.update(dt)
 langdon:update(dt)
-if love.keyboard.isDown('right') then
-    hun.body:applyForce(1000,0) 
+if love.keyboard.isDown('space') then
+    ball.body:applyForce(0,-1789)
+elseif love.keyboard.isDown('right') then
+     ball.body:applyForce(2000,0)
 elseif love.keyboard.isDown('left') then
-     hun.body:applyForce(- 1000,0)
-     end
-     end
+     ball.body:applyForce(-2000,0)
+end
+end
+function beginContact(a,b,coll)
+x,y = coll:getNormal()
+text = text .. "\n" .. a:getUserData() .. " colliding with " .. b:getUserData() .. "with a vector normal of: " .. x ..", " .. y
+end
+function endContact(a,b,coll)
+persisting = 0
+text = text .. "\n" .. a:getUserData() .. " uncolliding with " .. b:getUserData()
+end
+function preSolve(a,b,coll)
+      if persisting == 0 then 
+       text = text .. "\n" ..a:getUserData() .. " touching " .. b:getUserData()
+       elseif persisting < 20 then
+        text = text .. " " .. persisting
+      end
+      persisting = persisting + 1
+      end
+function endSolve(a,b,coll,normalimpulse,tangentimpulse)
+ end
