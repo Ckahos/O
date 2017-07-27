1514fc9c0023df99e01de3a021a58d0035d74d92
Ckahos <thialisson.caaci@gmail.com>
2017-07-27 12:23:00 -0300

    (The almost impossible game)

--- main.lua
+++ main.lua
@@ -1,50 +1,58 @@
+--#Game: The Almost impossible game.
+-- Vinicius, make the blocks be generated only after half the screen. 
 function love.load()
-love.window.setTitle("Demo #1")
-ll = love.physics.newWorld(0,145*12, true)
-love.physics.setMeter(12)
-love.window.setMode(800,640)
-love.graphics.setBackgroundColor(0,0,0)
+love.window.setFullscreen(true,"desktop")
 xw,yw = love.graphics.getDimensions()
-aa = {}
-aa.a = love.physics.newBody(ll,xw/10,yw*0.87,"dynamic")
-aa.s = love.physics.newCircleShape(25)
-aa.f = love.physics.newFixture(aa.a,aa.s)
-ss = {}
-ss.a = love.physics.newBody(ll,aa.a:getX(),aa.a:getY()+ 40)
-ss.s = love.physics.newRectangleShape(132,35)
-ss.f = love.physics.newFixture(ss.a,ss.s)
-ss.f:setRestitution(0.987)
+love.physics.setMeter(15)
+ll = love.physics.newWorld(0,100*15, true)
+a = {}
+a.a = love.physics.newBody(ll,xw/10,yw/2,"dynamic")
+a.s = love.physics.newCircleShape(15)
+a.f = love.physics.newFixture(a.a,a.s)
+a.f:setRestitution(0.987)
+v = {}
+v.a = love.physics.newBody(ll,a.a:getX(),a.a:getY() + 20,"static") -- Make this rotate.
+v.s = love.physics.newRectangleShape(200,15) 
+v.f = love.physics.newFixture(v.a,v.s)
+v.f:setRestitution(0.987)
+f = {}
+f.a = love.physics.newBody(ll,(xw/2) + 50,yw/2)
+f.s = love.physics.newRectangleShape(75,15)
+f.f = love.physics.newFixture(f.a,f.s)
 end
 function love.update(dt)
 ll:update(dt)
---if love.keyboard.isDown('right') then
- --          aa.a:setPosition(aa.a:getX() + 10 ,aa.a:getY())
---elseif love.keyboard.isDown('left') then
-  --  aa.a:setPosition(aa.a:getX() - 10 ,aa.a:getY())
-          
---elseif love.keyboard.isDown('up')  then
-    --     aa.a:setPosition(aa.a:getX(),aa.a:getY() - 10)
-  --elseif love.keyboard.isDown('down')  then
-       --  aa.a:setPosition(aa.a:getX(),aa.a:getY()+ 10)   
---end
+if love.keyboard.isDown('up') then
+      a.a:applyForce(0,-5000)
+elseif love.keyboard.isDown('down')  then
+     a.a:applyForce(0,5000)
+elseif love.keyboard.isDown('right')  then
+          a.a:applyForce(5000,0)
+elseif love.keyboard.isDown('left') then
+                a.a:applyForce(-5000,0)
+end
+pp()
+ss()
 end
 function love.draw()
-love.graphics.setColor(255,255,255,255)
-love.graphics.print(xw .. "--" .. yw)
-love.graphics.setColor(0,100,0)
-love.graphics.circle("fill",aa.a:getX(),aa.a:getY(),aa.s:getRadius())
-love.graphics.setColor(255,255,255)
-love.graphics.print(aa.a:getX().. "||".. aa.a:getY(),0,35)
-love.graphics.setColor(123,222,0)
-love.graphics.polygon("fill",ss.a:getWorldPoints(ss.s:getPoints()))
+love.graphics.setBackgroundColor(0,0,0)
+love.graphics.circle("line",a.a:getX(),a.a:getY(),a.s:getRadius())
+love.graphics.polygon("line",v.a:getWorldPoints(v.s:getPoints()))
+love.graphics.print(xw .. "||" .. yw)
+love.graphics.print(a.a:getX() .. "||" .. a.a:getY(),0,35)
+love.graphics.polygon("line",f.a:getWorldPoints(f.s:getPoints()))
 end
-function oo()
-local a = aa.a
-local s = aa.s
-local d = aa.f
-if aa.a:getX() >= 840  then
- aa.a:setPosition(-40,aa.a:getY())
-  elseif aa.a:getX() <= -40 then
-         aa.a:setPosition(800,aa.a:getY())
- end
-   end
+function pp()
+if a.a:getX() > xw  then
+         a.a:setPosition(-20,a.a:getY())
+elseif a.a:getX() < -10 then
+     a.a:setPosition(xw + 1, a.a:getY())  
+elseif a.a:getY() > yw  then
+        a.a:setPosition(a.a:getX(),- 20)
+elseif a.a:getY() < 0  then
+          a.a:setPosition(a.a:getX(),yw + 20) 
+end
+end
+function ss()
+f.a:setPosition(f.a:getX() - 2,f.a:getY())
+end
